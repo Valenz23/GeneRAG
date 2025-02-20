0299967961744926f3793476612f380992b0171a
Pablo Valenzuela Álvarez <valenbetis@gmail.com>
2025-02-20 12:24:00 +0100

    rag updated

--- _demos/ragdemo/query_data.py
+++ _demos/ragdemo/query_data.py
@@ -1,6 +1,7 @@
 from langchain_chroma import Chroma
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_ollama import ChatOllama
 
 import argparse
@@ -13,12 +14,11 @@ PROMPT_TEMPLATE = """
 Answer the question based only on the following context: {context}
 ---
 Answer the question based on the above context: {question}
----
-Keep this following format in your response --> Response: your response \nSources: {sources}
 """
 
 
 def main():
+
     # Create CLI.
     parser = argparse.ArgumentParser()
     parser.add_argument("query_text", type=str, help="The query text.")
@@ -28,6 +28,7 @@ def main():
 
 
 def query_rag(query_text: str):
+    
     # Prepare the DB.
     embedding_function = get_embedding_function()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
@@ -35,18 +36,22 @@ def query_rag(query_text: str):
     # Search the DB.
     results = db.similarity_search_with_score(query_text, k=5)
     context = "\n\n---\n\n".join([doc.page_content for doc, _score in results])    
-    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    sources = [doc.metadata.get("id", None) for doc, _score in results]    
     
     # Prompt and chain
     prompt = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    model = ChatOllama(model="llama3.2", base_url="http://localhost:11434")
-    chain = prompt | model | StrOutputParser()    
+    llm = ChatOllama(model="llama3.2", base_url="http://localhost:11434")     
     
-    # Streaming response
+    chain = prompt | llm | StrOutputParser()
+
+    print("Response:", end=" ")
     response_text = ""
-    for chunk in chain.stream({"context": context, "sources": sources, "question": query_text}):
+    for chunk in chain.stream({"context": context, "question": query_text}):
         print(chunk, end="", flush=True)
-        response_text += chunk  
+        response_text += chunk      
+    
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    print(f"\nSources: {sources}")
 
     return response_text
 
