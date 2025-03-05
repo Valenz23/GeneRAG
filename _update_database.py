51c7ff75a8a2249bfbfa9777cfa5a8cbac0c7121
Pablo Valenzuela Álvarez <valenbetis@gmail.com>
2025-03-05 13:37:36 +0100

    updated project
    he cambiado el nombre de la carpeta data/html --> data/web
    he creado un miniset para hacer pruebas
    he creado más metodos de carga de documentos para la DB

--- _update_database.py
+++ _update_database.py
@@ -1,14 +1,24 @@
 
 
 from functions.add_to_chroma import add_to_chroma
-from functions.get_document_loader import load_pdf_documents
+from functions.get_document_loader import load_pdf_documents, load_xml_documents, load_web_documents, load_web_documents2
 from functions.get_embedding_function import get_embedding_function
 from functions.get_text_spliter import split_text
 
 from enum import Enum
+from timeit import default_timer as timer
 
 CHROMA_PDF_PATH = "chroma/pdf"
-DATA_PDF_PATH = "data/pdf"
+CHROMA_XML_PATH = "chroma/xml"
+CHROMA_WEB_PATH = "chroma/web"
+
+# DATA_PDF_PATH = "data/pdf"
+# DATA_XML_PATH = "data/xml"
+# DATA_WEB_PATH = "data/web"
+
+DATA_PDF_PATH = "miniset/pdf"
+DATA_XML_PATH = "miniset/xml"
+DATA_WEB_PATH = "miniset/web"
 
 class EMBEDDING(Enum):
     NOMIC = "nomic-embed-text"
@@ -17,12 +27,24 @@ class EMBEDDING(Enum):
     JINA = "jina/jina-embeddings-v2-base-es"
 
 def main():
-    documents = load_pdf_documents(DATA_PDF_PATH)
+    start = timer()
+
+    # documents = load_pdf_documents(DATA_PDF_PATH)
+    # documents = load_xml_documents(DATA_XML_PATH)    
+    documents = load_web_documents(DATA_WEB_PATH)
+
+    # documents = load_web_documents2(DATA_XML_PATH) # DELETE
+
+    chunks = split_text(documents, 500, 50)
+
     # for doc in documents:
     #     print(doc.metadata)
-    chunks = split_text(documents, 500, 50)
-    # print(chunks)
-    add_to_chroma(chunks, CHROMA_PDF_PATH, get_embedding_function(model=EMBEDDING.NOMIC))
+    print(chunks)
+
+    end = timer()
+    print("%.2fs" % (end-start))
+
+    # add_to_chroma(chunks, CHROMA_PDF_PATH, get_embedding_function(model=EMBEDDING.NOMIC))
 
 if __name__ == "__main__":
     main()
