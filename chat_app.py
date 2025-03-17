4b0f90b1a994d72e658ce2227fbba461931fdef0
Pablo Valenzuela Álvarez <valenbetis@gmail.com>
2025-03-17 10:51:45 +0100

    add hf_models
    added some huggingface models

--- chat_app.py
+++ chat_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 from functions.get_chroma_db import get_chroma_db
 from functions.get_embedding_function import get_embedding_function
-from functions.get_llm_model import get_llm_model
+from functions.get_llm_model import get_llm_model, get_hf_model
 
 import random
 from enum import Enum
@@ -15,18 +15,28 @@ CHROMA_XML_PATH = "chroma/xml"
 CHROMA_WEB_PATH = "chroma/web"
 
 class LLM(Enum):
-    LLAMA32 = "llama3.2"        # meta 3b 2gb
-    DEEPSEEKR1 = "deepseek-r1"  #ds 7b 4.7gb
-    GEMMA3 = "gemma3"           # google dm 4b 3.3gb
-    QWEN25 = "qwen2.5"          # alibaba 7b 4.7gb
-    MISTRAL = "mistral"         # mistral ai 7b 4.1gb    
-    HERMES3 = "hermes3"         # nous research 8b 4.7gb
+    llama_3_2_3B = "llama3.2"        # meta 3b 2gb
+    deepseek_R1_7B = "deepseek-r1"  #ds 7b 4.7gb
+    gemma3_4B = "gemma3"           # google dm 4b 3.3gb
+    qwen_2_5_7B = "qwen2.5"          # alibaba 7b 4.7gb
+    mistral_7B = "mistral"         # mistral ai 7b 4.1gb    
+    hermes_3_8B = "hermes3"         # nous research 8b 4.7gb
+
+class LLM_HF(Enum):
+    meta_llama32_3B_Instruct = "meta-llama/Llama-3.2-3B-Instruct"    # 3.21B params
+    mistral_7B_Instruct_v03 ="mistralai/Mistral-7B-Instruct-v0.3"   # 7.25B params
+    Qwen_QwQ_32B ="Qwen/QwQ-32B"                         # 32.8B params // overloaded
+    deepseekR1_Distill_Qwen_32B= "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B" # 32.8B params
+    # deephermes3_llama_3_3B= "NousResearch/DeepHermes-3-Llama-3-3B-Preview" # 3.21B params // temporary unavailable
+    # phi_4_mini_instruct = "microsoft/Phi-4-mini-instruct"   # 3.84B params // temporary unavailable   
+    # gemma3_4b = "google/gemma-3-4b-pt"  # 4.3B params // temporary unavailable
+    
 
 class EMBEDDING(Enum):
     NOMIC = "nomic-embed-text"                  # nomic team
     MXBAI = "mxbai-embed-large"                 # mixed bread
-    SNOWFLAKEv2 = "snowflake-arctic-embed2"     # snowflake
-    JINA = "jina/jina-embeddings-v2-base-es"    # jina ai
+    Snowflake = "snowflake-arctic-embed2"     # snowflake
+    Jina = "jina/jina-embeddings-v2-base-es"    # jina ai
 
 PROMPT_TEMPLATE = """
 Responde la pregunta: {question}
@@ -35,6 +45,7 @@ Tienes que basarte ÚNICAMENTE en el siguiente contexto: {context}
 Todas las preguntas que haga el usuario estarán relacionadas con la DANA ocurrida en España.  
 Debes proporcionar una respuesta detallada y bien estructurada, organizando la información en párrafos y listas si es necesario.  
 Si la respuesta no se encuentra en el contexto, indícalo claramente y no inventes datos.
+Escribe SOLO la respuesta, no escribas NUNCA "segun el contexto" o cosas parecidas.
 """
 
 
@@ -42,7 +53,7 @@ WELCOME_MESSAGES = [
     "Mensaje de bienvenida de prueba.",
 ]
 
-def query(question: str, sel_llm: str):
+def query(question: str, source_model: str, sel_llm: str):
 
     # conexion base de datos y consulta
     db = get_chroma_db(CHROMA_PDF_PATH, get_embedding_function(model=EMBEDDING.NOMIC))
@@ -70,7 +81,13 @@ def query(question: str, sel_llm: str):
     
     # Prompt & chain
     prompt = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    llm = get_llm_model(model=sel_llm)
+
+    llm = ""
+    if source_model == "Local":
+        llm = get_llm_model(sel_llm)
+    else:
+        llm = get_hf_model(sel_llm)
+
     chain = prompt | llm | StrOutputParser()
 
     # generacion de respuesta
@@ -88,9 +105,21 @@ def main():
     st.markdown("<h1 style='text-align: center;'>¡Bienvenido!</h1>", unsafe_allow_html=True)
     st.markdown("<h2 style='text-align: center;'>Usa este chat para obtener información</h2>", unsafe_allow_html=True)
 
+    source_model = st.sidebar.radio(
+        "Elige la fuente del modelo",
+        ["Local", "HuggingFace"],
+        index=0
+        )
+    
+    models = ""
+    if source_model == "Local":
+        models = [model.value for model in LLM]
+    else:
+        models = [model.value for model in LLM_HF]
+
     select_llm = st.sidebar.selectbox(
-        label="Selecciona un modelo",
-        options=([model.value for model in LLM]),        
+        label="Modelos disponibles",
+        options=models,        
         placeholder="Seleccione una opción",
         index=0
     )
@@ -116,7 +145,7 @@ def main():
             
         with st.chat_message("AI"):
             with st.spinner("Pensando ...", show_time=True):
-                response = query(user_query, select_llm)
+                response = query(user_query, source_model, select_llm)
             
         st.session_state.chat_history.append(AIMessage(content=response))   
 
--- functions/get_llm_model.py
+++ functions/get_llm_model.py
@@ -1,12 +1,33 @@
 from langchain_ollama import OllamaLLM, ChatOllama
+from langchain_huggingface import HuggingFaceEndpoint
+
+from dotenv import load_dotenv
+import os
+import warnings
+
+# warnings.filterwarnings("ignore", category=FutureWarning)
+load_dotenv()
+HUGGINGFACEHUB_API_TOKEN = os.getenv('HUGGINGFACEHUB_API_TOKEN')
 
 def get_llm_model(model):
     return OllamaLLM(
         model=model,      
-        num_ctx=2048
+        # num_ctx=2048
     )  
 
 def get_chat_model(model):
     return ChatOllama(
         model=model
     )  
+
+def get_hf_model(model):
+    return HuggingFaceEndpoint(
+        endpoint_url=model,
+        # max_new_tokens=512,
+        top_k=10,
+        top_p=0.95,
+        typical_p=0.95,
+        temperature=0.01,
+        repetition_penalty=1.03,
+        huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN
+    )
