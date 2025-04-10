cdeab2a622c92dc5da3fb66570e96e7899734f11
Pablo Valenzuela Álvarez <valenbetis@gmail.com>
2025-04-10 12:15:55 +0200

    eval rag
    prompt improved, but sometimes it doesn't extract the good response ...

--- classes/chatbot.py
+++ classes/chatbot.py
@@ -10,10 +10,10 @@ from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
 from timeit import default_timer as timer
 
-# from dotenv import load_dotenv
-# import os
-# load_dotenv()
-# MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
+from dotenv import load_dotenv
+import os
+load_dotenv()
+MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
 
 # CHROMA_PDF_PATH = "chroma/pdf"
 # CHROMA_XML_PATH = "chroma/xml"
@@ -36,8 +36,8 @@ class Chatbot:
                  docs_directory: str = "my_data"                        # directorio de documentos                 
                  ):
         
-        self.language_model = ChatOllama(model=language_model, num_ctx=num_ctx, temperature=0.1, seed=12345)        
-        # self.language_model = ChatMistralAI(model="mistral-small-latest", mistral_api_key=MISTRAL_API_KEY,temperature=0.1, random_seed=12345)
+        # self.language_model = ChatOllama(model=language_model, num_ctx=num_ctx, temperature=0.1, seed=12345)        
+        self.language_model = ChatMistralAI(model="mistral-small-latest", mistral_api_key=MISTRAL_API_KEY,temperature=0.1, random_seed=12345)
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap, length_function=len)        
         self.embedding_service = OllamaEmbeddings(model=embedding_model)        
         self.vector_store = Chroma(persist_directory=chroma_directory, embedding_function=self.embedding_service)        
@@ -149,6 +149,15 @@ class Chatbot:
             | StrOutputParser()
         )
         return query_chain.stream({"question": query, "context": context})
+    
+    def answer_query2(self, query: str, context: str):
+        query_chain = (
+            {"context": self.retriever, "question": RunnablePassthrough(), "context": RunnablePassthrough()}
+            | self.prompt_template
+            | self.language_model
+            | StrOutputParser()
+        )
+        return query_chain.invoke({"question": query, "context": context})
     
     
     # def load_web_documents(path):    
